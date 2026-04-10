77b0bd36ae46afdefc324aa76ae0aa74229570a0
willfulDU <leo657290@gmail.com>
2026-04-10 22:06:56 +0800

    【3车高腿pid】

--- code/plan/bridge.h
+++ code/plan/bridge.h
@@ -25,20 +25,20 @@
 #if CAR_SELECT == 3 // 3代表 【2026/3/30新车】 对应板子 【2026/03/24 最后的舵机v腿】
 // 【单边桥高姿态专属 PID 参数】 (用于抬腿时的线性插值，防止原地震荡)
 // 中环 (角度环) : 抬高后惯性变大，需要稍微加大推力，大幅增加阻尼
-#define ANG_KP_HIGH          0.0f   // [原20.0] 微增
-#define ANG_KD_HIGH          0.0f   // [原8.0]  显著增加阻尼，防止荡秋千
-#define ANG_MECH_ZERO_HIGH   0.0f    // [原2.6]  实车需重新测定最高姿态时的绝对平衡点
+#define ANG_KP_HIGH          -35.0f   // 微增
+#define ANG_KD_HIGH          -10.0f   //  显著增加阻尼，防止荡秋千
+#define ANG_MECH_ZERO_HIGH   -3.5f    // 实车需重新测定最高姿态时的绝对平衡点
 
 // 内环 (角速度环) : 抬高后机械变软，减小响应防止高频共振“嗡嗡”响
-#define GYR_KP_HIGH          0.0f  // [原-18.0] 减小绝对值
+#define GYR_KP_HIGH          -11.0f  //  减小绝对值
 
 //舵机速度环
-#define SERVO_SPEED_KP_HIGH         0.0f   // [原-6.5] 微减
-#define SERVO_SPEED_KI_HIGH   0.0f    // [原-0.03]
+#define SERVO_SPEED_KP_HIGH         -6.5f   // 微减
+#define SERVO_SPEED_KI_HIGH   -0.08f    // 
 
 // 转向环 : 重心变高容易侧翻，转向必须变柔和
-#define TURN_ANG_KP_HIGH     0.0f   // [原-12.0] 减小绝对值
-#define TURN_GYR_KP_HIGH     0.0f   // [原-3.0]  减小绝对值
+#define TURN_ANG_KP_HIGH     0.0f   //  减小绝对值
+#define TURN_GYR_KP_HIGH     0.0f   //  减小绝对值
 #endif
 // ============================================================================
 // 单边桥状态机枚举
