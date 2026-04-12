d2d8bdbb914b9b756f1b9e924c03c8b8a2728e8a
willfulDU <leo657290@gmail.com>
2026-04-12 15:57:03 +0800

    【开启rolling】

--- code/tools/sbus.c
+++ code/tools/sbus.c
@@ -208,7 +208,9 @@ void Remote_Control_Process(void)
     }
     if (curr_ch3_state != last_ch3_state )
     {
-    robot_ctrl.mark_trigger = 1; // 置位，Main函数处理完需手动清零
+        vision_detected_jump_point = 1;//跳跃点调用,测试用
+        //vision_detected_bumpy_point = 1;//颠簸路段调用,测试用
+    //robot_ctrl.mark_trigger = 1; // 置位，Main函数处理完需手动清零
     // NAV_POINT_PATH = 0,     // 普通路径点
     // NAV_POINT_CIRCLE = 1,   // 转圈点
     // NAV_POINT_SLOPE = 2,    // 上坡点
