a45d445d2a90e48c8d9582943c8b903ed05720fc
Boatchanting <1440068386@qq.com>
2026-04-29 19:24:29 +0800

    【config】更新CURRENT_NAV_PLAN配置，优化科目一和科目二的状态机逻辑

--- code/config/sys_options.h
+++ code/config/sys_options.h
@@ -9,6 +9,15 @@
 #define IMU_CATEGORY 3//【全局开关】1:imu660ra  2:imu660rb 3:imu963ra 注：imu660ra被赛事禁用
 #define SUBS_CATEGORY 1  //【遥控器选择】1.旧遥控器2.新遥控器。选反会导致前进后退相反
 // ---------------- plan 配置 ----------------
-#define CURRENT_NAV_PLAN   NAV_PLAN_1   // 【全局开关】【暂时未使用】在这里切换科目几，科目一为NAV_PLAN_1，科目二NAV_PLAN_2，科目三NAV_PLAN_3
+#define CURRENT_NAV_PLAN   2   // 【全局开关】在这里切换科目几，科目一为1，科目二2，科目三3，每个科目的主要逻辑会单独优化，上层控制参数层不共享，现阶段尽量做到互不干扰(0429)，后面做到各自独立优化
+/*
+【科目一优化与拆分】
+1.1.惯导条件下，去除了任务点搜索code/navigation/nav_replay.c
+1.2.
 
+【科目二优化与拆分】
+2.1.惯导条件下，状态机触发不用先角度对准code/navigation/nav_replay.c
+2.2.惯导条件下，状态机进保留了雷区状态机code/navigation/nav_replay.c
+
+*/
 #endif // __SYS_OPTIONS_H__
