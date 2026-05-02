8bfb0bc06475ef9fc1a3b52c879c6e7107396f00
Boatchanting <1440068386@qq.com>
2026-05-02 20:31:17 +0800

    【vision】重命名单边桥任务视觉控制模块及相关文件引用

--- user/main_cm7_0.c
+++ user/main_cm7_0.c
@@ -39,7 +39,7 @@
 #include "vision/vision_ipc_core0.h"
 #include "vision/vision_pvc_control.h"
 #include "vision/vision_bumpy_control.h"
-#include "vision/vision_task_area.h"
+#include "vision/vision_bridge_control.h"
 
 
 // **************************** uart≈‰÷√«¯”Ú **************************** 
