8e4140af14e0ccca081a61292107626e50f984a5
cxzzz <112432598@qq.com>
2026-05-06 20:51:07 +0800

    【gnss】校准gnss地理北偏差

--- code/navigation/nav_replay.h
+++ code/navigation/nav_replay.h
@@ -14,7 +14,7 @@
 #define GPS_NAV_DIST_NEAR                 2200.0f
 #define GPS_NAV_DIST_FAR                  5000.0f
 #define GPS_NAV_YAW_TOLERANCE             8.0f
-#define GPS_NAV_HEADING_OFFSET_DEG        0.0f
+#define GPS_NAV_HEADING_OFFSET_DEG        -158.23f
 #define GPS_NAV_SPEED_FAST                -150.0f
 #define GPS_NAV_SPEED_SLOW                -20.0f
 #define GPS_NAV_SPEED_STOP                NAV_SPEED_STOP
