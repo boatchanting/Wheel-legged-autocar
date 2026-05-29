185474cd948ac1d60ebe66e170667b42deb7aa68
Boatchanting <1440068386@qq.com>
2026-05-29 17:56:54 +0800

    【科目三】联合调试，摄像头固定阈值，寻迹方法有误

--- code/config/sys_options.h
+++ code/config/sys_options.h
@@ -16,13 +16,13 @@
 #define ACCEL_FF_ENABLE 1U // 【加速前馈总开关】1:启用复刻起步/急加速前馈  0:完全关闭加速前馈
 #define ACCEL_FF_MODE 1U // 【加速前馈模式】0:关闭  1:直接叠加 PWM 前馈  2:加速时临时增强舵机速度环 Kp
 #define ACCEL_FF_BUZZER_ENABLE 1U // 【全局开关】1:大幅加速前馈触发时蜂鸣提示  0:关闭
-#define ROLL_BALANCE_ENABLE_INIT 1U //【全局开关】主动侧倾平衡环初始使能状态，1为上电默认使能，0为上电默认关闭
+#define ROLL_BALANCE_ENABLE_INIT 0U //【全局开关】主动侧倾平衡环初始使能状态，1为上电默认使能，0为上电默认关闭
 #define IMU_CATEGORY 3//【全局开关】1:imu660ra  2:imu660rb 3:imu963ra 注：imu660ra被赛事禁用
 #define IMU_REFRESH_TEST_ENABLE 0 // 1: 上电后测试IMU刷新频率，运行10秒后串口打印一次结果
 #define SUBS_CATEGORY 1  //【遥控器选择】1.旧遥控器2.新遥控器。选反会导致前进后退相反
 // ---------------- plan 配置 ----------------
 #define GNSS_NAV 0 // 【全局开关】gps寻迹还是惯导寻迹，现阶段暂时还没联合(date0511)，联合后考虑去除该开关，1表示使用gnss寻迹，0表示不使用gnss寻迹，惯导开关常开
-#define CURRENT_NAV_PLAN   2   // 【全局开关】在这里切换科目几，科目一为1，科目二2，科目三3，nav_replay模版函数99，每个科目的主要逻辑会单独优化，上层控制参数层不共享，互不干扰，后面做到各自独立优化，这个开关现在对惯导寻迹和gps方案均有效(date0520)
+#define CURRENT_NAV_PLAN   3   // 【全局开关】在这里切换科目几，科目一为1，科目二2，科目三3，nav_replay模版函数99，每个科目的主要逻辑会单独优化，上层控制参数层不共享，互不干扰，后面做到各自独立优化，这个开关现在对惯导寻迹和gps方案均有效(date0520)
 /*
 【科目一优化与拆分】
 1.1.惯导条件下，去除了任务点搜索code/navigation/nav_replay.c
