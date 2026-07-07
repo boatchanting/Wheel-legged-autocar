014667f98315cc600300cf15a81670f2c248c0b0
willfulDU <leo657290@gmail.com>
2026-07-07 20:40:16 +0800

    【option】选择了0核自定义

--- code/config/sys_options.h
+++ code/config/sys_options.h
@@ -2,8 +2,8 @@
 #define __SYS_OPTIONS_H__
 
 #define WIFI_USE 1 // 【WIFI总开关】选择是否使用WIFI模块，0表示不使用，1表示使用
-#define WIFI_CORE_SELECT 1 // 【WIFI核心选择】0表示0核使用WIFI，1表示1核使用WIFI
-#define WIFI_PROTOCOL_SELECT 1 // 【WIFI协议选择】1表示逐飞助手，2表示我们的自定义协议
+#define WIFI_CORE_SELECT 0 // 【WIFI核心选择】0表示0核使用WIFI，1表示1核使用WIFI
+#define WIFI_PROTOCOL_SELECT 2 // 【WIFI协议选择】1表示逐飞助手，2表示我们的自定义协议
 #define G_MOTOR_ENABLE_INIT 1 // 【电机使能初值】控制g_motor_enable上电默认状态，1为使能，0为关机
 #define DEBUG_DISPLAY 1                  // 【全局开关】1:开启屏幕调试显示  0:关闭
 #define DEBUG_DISPLAY_CORE_SELECT 0      // 【显示核心选择】0: 0核独占屏幕  1: 1核视觉屏幕
@@ -22,7 +22,7 @@
 #define SUBS_CATEGORY 1  //【遥控器选择】1.旧遥控器2.新遥控器。选反会导致前进后退相反
 // ---------------- plan 配置 ----------------
 #define GNSS_NAV 0 // 【全局开关】gps寻迹还是惯导寻迹，现阶段暂时还没联合(date0511)，联合后考虑去除该开关，1表示使用gnss寻迹，0表示不使用gnss寻迹，惯导开关常开
-#define CURRENT_NAV_PLAN   2   // 【全局开关】在这里切换科目几，科目一为1，科目二2，科目三3，nav_replay模版函数99，每个科目的主要逻辑会单独优化，上层控制参数层不共享，互不干扰，后面做到各自独立优化，这个开关现在对惯导寻迹和gps方案均有效(date0520)
+#define CURRENT_NAV_PLAN   1   // 【全局开关】在这里切换科目几，科目一为1，科目二2，科目三3，nav_replay模版函数99，每个科目的主要逻辑会单独优化，上层控制参数层不共享，互不干扰，后面做到各自独立优化，这个开关现在对惯导寻迹和gps方案均有效(date0520)
 /*
 【科目一优化与拆分】
 1.1.惯导条件下，去除了任务点搜索code/navigation/nav_replay.c
