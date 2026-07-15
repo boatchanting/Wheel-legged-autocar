c001eebc2af1afd99b88b14e1c9880fe42deb8dc
Boatchanting <1440068386@qq.com>
2026-07-15 20:57:45 +0800

    【main1】更新IPC接口函数名称以支持桥梁重置请求和处理

--- user/main_cm7_1.c
+++ user/main_cm7_1.c
@@ -136,7 +136,7 @@ int main(void)
             {
                 pvc_vision_reset_filter();
             }
-            if(VisionIpc_Core1_TakeLineResetRequest())
+            if(VisionIpc_Core1_TakeBridgeResetRequest())
             {
                 bridge_vision_reset_filter();
             }
@@ -153,7 +153,7 @@ int main(void)
                 // 4. 将 PVC 检测框直接画在 compressed_image_copy[0] 上，供 WIFI 发送显示
                 render_pvc_vision_to_image();//算法执行完毕后，将 PVC 检测框画在 image_copy 上,必须放在这！如果放在算法前面，画的黑线会破坏算法寻找白色的逻辑
             }
-            if(VisionIpc_Core1_ShouldRunBridgeLine())
+            if(VisionIpc_Core1_ShouldRunBridge())
             {
                 bridge_vision_process_camera_frame(compressed_image_copy[0]);
                 render_bridge_vision_to_image();
