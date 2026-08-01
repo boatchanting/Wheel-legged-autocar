cc29f1b4971603aee0bb5fa64d39e8d42d4a6722
willfulDU <leo657290@gmail.com>
2026-08-01 20:51:42 +0800

    【plan2】22秒

--- code/navigation/nav_replay_route_table.h
+++ code/navigation/nav_replay_route_table.h
@@ -4,22 +4,22 @@
 #include "nav_ram.h"
 
 // 由 tools/webview_nav_marker速度规划_科目二/csv_to_nav_table.py 自动生成
-// 源 CSV：D:/autocar/project3/project3/project/tools/webview_nav_marker速度规划_科目二/nav_mark_points_20260729_103533.csv
-// 生成时间：2026-07-29 10:35:44
+// 源 CSV：D:/New_luntui/project3/project/tools/webview_nav_marker速度规划_科目二/nav_mark_points_20260801_145113.csv
+// 生成时间：2026-08-01 20:47:20
 
 #define NAV_REPLAY_START_HEADING_VALID 0
-#define NAV_REPLAY_START_HEADING_DEG 59.542f
+#define NAV_REPLAY_START_HEADING_DEG 51.075f
 
 #define NAV_REPLAY_STATIC_ROUTE_COUNT 7
 
 static const NavRamPoint_t nav_replay_static_route_points[7] = {
-    {-5019.906f, -1448.393f, 16.519f, 80.558f, (uint8)1, 0.000f},
-    {-8461.745f, -3655.490f, 33.232f, 86.387f, (uint8)1, 0.000f},
-    {-12367.769f, -1201.299f, -30.876f, 116.894f, (uint8)1, 0.000f},
-    {-13942.402f, -3900.000f, 54.664f, 88.507f, (uint8)1, 0.000f},
-    {-16590.496f, -4059.653f, 3.766f, 75.551f, (uint8)6, 0.000f},
-    {-18484.326f, -2893.368f, -48.417f, 134.725f, (uint8)6, 0.000f},
-    {-16896.352f, -583.237f, -175.170f, 121.782f, (uint8)6, 0.000f},
+    {-4740.621f, -0.236f, 0.143f, 91.397f, (uint8)1, 0.000f},
+    {-5186.450f, -4461.109f, 86.102f, 113.702f, (uint8)1, 0.000f},
+    {-8056.352f, -2321.229f, -34.089f, 87.237f, (uint8)1, 0.000f},
+    {-12105.081f, -2508.037f, 1.866f, 91.987f, (uint8)1, 0.000f},
+    {-21228.918f, -2785.657f, 1.666f, 91.883f, (uint8)6, 0.000f},
+    {-23173.377f, -1810.672f, -75.177f, 149.789f, (uint8)6, 0.000f},
+    {-22605.998f, 34.897f, 179.363f, 132.584f, (uint8)6, 0.000f},
 };
 
 #endif // _NAV_REPLAY_ROUTE_TABLE_H_
--- code/plan/minefield.c
+++ code/plan/minefield.c
@@ -7,9 +7,9 @@ extern uint8 g_special_action_trigger;
 // 旋转总角度下限（deg）；外部即使给得更小，也会被钳到这个值。
 #define SPIN_TARGET_ANGLE_MIN     MINEFIELD_SPIN_MIN_TOTAL_ANGLE
 // 旋转阶段的最大角速度指令（deg/s）。
-#define SPIN_MAX_SPEED            1000.0f
+#define SPIN_MAX_SPEED            1300.0f   //华东调试//角速度指令上限
 // 角速度爬升斜率；避免转圈动作起转过猛。
-#define SPIN_ACCEL_STEP           1.5f
+#define SPIN_ACCEL_STEP           1.8f
 // 减速区角度（deg）；进入最后这段角度后开始线性收速。
 #define SPIN_DECEL_ANGLE          120.0f
 // 旋转末段的最小角速度指令（deg/s）；避免末段因速度过低卡住。
